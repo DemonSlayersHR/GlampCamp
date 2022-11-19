7cb713fa320bf70db3fad2e4a1ecddbce10c2a02
RandyIp <randyip9@mail.com>
2022-11-19 09:45:11 -0800

    Saving b4 pulling

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require('express')
 const path = require('path')
-
 const app = express()
+
+const server = app.listen(3000)
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const cors = require('cors')
 const morgan = require('morgan')
 const router = require('./router.js')
@@ -13,6 +17,17 @@ app.use(express.json());
 // Uncomment/edit below to serve static site
 // app.use(express.static(path.join(__dirname, '../client/dist')))
 
-app.use('/', router)
-app.listen(process.env.PORT || 3000);
+// uncomment below to test socketio
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/socketio.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+// app.use('/', router)
+// app.listen(process.env.PORT || 3000);
 console.log(`Listening at http://localhost:${process.env.PORT || 3000}`);
