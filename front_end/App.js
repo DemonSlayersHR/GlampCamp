405d73e7d1bdf43684ea3335d96a153b11ff3433
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2022-11-19 10:04:59 -0800

    please

---
+++ front_end/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Randy will build this out for us! 🎉 !!!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
