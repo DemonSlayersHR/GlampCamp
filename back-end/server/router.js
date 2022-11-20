e128d3c670a593a5d9f758d348625b103fcf581e
Monica Liu <liuqianmonica@gmail.com>
2022-11-19 18:35:27 -0800

    implement get user api

--- back-end/server/router.js
+++ back-end/server/router.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const users = require('./Controllers/Monica/userController.js');
+
+router.get('/user',users.getUser)
 
 module.exports = router
