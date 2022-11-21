103c406058627ed6712d1ba86582c19cf4a1c9c2
Scott Matsuda <smatsuda2014@gmail.com>
2022-11-21 10:10:12 -0800

    fixed image bug in SingleCampsite and added props to Calendar

--- front_end/src/screens/campsite/SingleCampsite.js
+++ front_end/src/screens/campsite/SingleCampsite.js
@@ -1,37 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import { Button, Image, Text, View, ScrollView } from 'react-native';
 import axios from 'axios';
+import Calendar from './Calendar.js';
 
 const SingleCampsite = ({campsite}) => {
 
-  console.log('campsite', campsite);
+  const [availabilityButtonClicked, setAvailabilityButtonClicked] = useState(false);
 
   const image = {
     uri: campsite.photos[1]?.photo_url
   }
 
-  console.log('image', image)
+  const showCalendar = () => {
+    setAvailabilityButtonClicked(true);
+  }
 
   return (
-      <ScrollView style={{padding: 69, flex: 1}}>
-        <View style={{justifyContent: "center", alignItems: "center"}} >
-          {campsite.photos[1] && <Image source={image} />}
-          <Text style={{fontSize: 20, padding:5, fontWeight: "bold", textAlign: "left", marginLeft:50,  marginRight:37}}>
-            {campsite.camp_name} - {campsite.description}
-          </Text>
-          <Text style={{textDecorationLine: "underline", textAlign: "center"}}>
-            {campsite.location}
-          </Text>
-          <Text>
-            {campsite.price} night, {campsite.star_rating} stars
-          </Text>
-          <Button
-            title="Reserve"
-            onPress={() => Alert.alert('Simple Button pressed')}
-          />
-        </View>
-      </ScrollView>
-
+    <ScrollView style={{padding: 69, flex: 1}}>
+      <View style={{justifyContent: "center", alignItems: "center"}} >
+        <Image source={image} style={{width: 400, height: 400}} />
+        <Text style={{fontSize: 20, padding:5, fontWeight: "bold", textAlign: "left", marginLeft:50,  marginRight:37}}>
+          {campsite.camp_name} - {campsite.description}
+        </Text>
+        <Text style={{textDecorationLine: "underline", textAlign: "center"}}>
+          {campsite.location}
+        </Text>
+        <Text>
+          {campsite.price} night, {campsite.star_rating} stars
+        </Text>
+        <Button
+          title="Show Availability"
+          onPress={showCalendar}
+        />
+        {availabilityButtonClicked && <Calendar />}
+      </View>
+    </ScrollView>
   );
 }
 
