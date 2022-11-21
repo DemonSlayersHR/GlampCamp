ccb3cf7d44514624dc16fb91c7ded9d08db625b7
RandyIp <randyip9@mail.com>
2022-11-20 23:45:38 -0800

    quick update on get campsite

--- back-end/server/Controllers/Randy/Campsite.js
+++ back-end/server/Controllers/Randy/Campsite.js
@@ -3,40 +3,40 @@ pool = require('../../../db/db.js')
 const campsite = {
   get: async (req, res) => {
 
-    // catch error on client
-    // if client idle end process
-    // pool.on('error', (err, client) => {
-    //   console.error('Unexpected error on idle client', err)
-    //   process.exit(-1)
-    // })
-
     // creates the client
     const client = await pool.connect()
 
     // create parameters
-    const queryArgs = [req.query.count || 10]
+    let queryArgs = [req.query.count || 10]
+
+    if (req.query.camp_id) {
+      queryArgs = [req.query.camp_id]
+    }
+
+    let query = `SELECT camp_name, (SELECT user_name FROM users WHERE user_id = camps.host_id) as host, price,
+    (SELECT AVG(star_rating) FROM reviews WHERE camp_id = camps.camp_id) as star_rating, location, description,
+    (SELECT json_agg(json_build_object(
+      'client', (SELECT user_name FROM users WHERE user_id = camp_dates.client_id),
+      'date', dates,
+      'reserved', reserved
+    )) as dates FROM camp_dates WHERE camp_id = camps.camp_id),
+    (SELECT json_agg(json_build_object(
+      'id', photo_id,
+      'photo_url', photo_url)) FROM photos WHERE camp_id = camps.camp_id
+    ) as photos,
+    (SELECT json_agg(json_build_object(
+      'reviewer', (SELECT user_name FROM users WHERE user_id = reviews.client_id),
+      'star_rating', star_rating,
+      'review', review
+    )) FROM reviews WHERE camp_id = camps.camp_id) as reviews
+    FROM camps `
+
+    if (req.body.camp_id) query += `WHERE camp_id = $1`
+    else query += `LIMIT $1`
 
     // try query
     try {
-      const result = await client.query(
-        `SELECT camp_name, (SELECT user_name FROM users WHERE user_id = camps.host_id) as host, price,
-        (SELECT AVG(star_rating) FROM reviews WHERE camp_id = camps.camp_id) as star_rating, location, description,
-        (SELECT json_agg(json_build_object(
-          'client', (SELECT user_name FROM users WHERE user_id = camp_dates.client_id),
-          'date', dates,
-          'reserved', reserved
-        )) as dates FROM camp_dates WHERE camp_id = camps.camp_id),
-        (SELECT json_agg(json_build_object(
-          'id', photo_id,
-          'photo_url', photo_url)) FROM photos WHERE camp_id = camps.camp_id
-        ) as photos,
-        (SELECT json_agg(json_build_object(
-          'reviewer', (SELECT user_name FROM users WHERE user_id = reviews.client_id),
-          'star_rating', star_rating,
-          'review', review
-        )) FROM reviews WHERE camp_id = camps.camp_id) as reviews
-        FROM camps
-      LIMIT $1`, queryArgs)
+      const result = await client.query(query, queryArgs)
 
       // reformat response and send
       res.status(200)
