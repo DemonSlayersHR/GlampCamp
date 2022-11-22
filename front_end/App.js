ecafbf095dbe96e257fd784bfa41bc516b29f549
Sean McEntagart <seanmcentagart@Seans-MacBook-Pro.local>
2022-11-21 20:23:21 -0800

    built out login and register screens, can navigate between both. firebase next

--- front_end/App.js
+++ front_end/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import SingleCampsite from './src/screens/campsite/SingleCampsite.js';
 import Campsites from './src/screens/homepage/Homepage.js';
+import Login from './src/screens/login_signup/Login.js';
+import Register from './src/screens/login_signup/Register.js';
 // import Calendar from './src/screens/campsite/Calendar.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='login'
+          component={Login}
+          options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen
+          name='register'
+          component={Register}
+          options={{ headerShown: false }}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
     // <View style={styles.container}>
     //   <Text>
     //     Randy will build this out for us! 🎉 !!!
@@ -13,7 +32,6 @@ export default function App() {
     //   <StatusBar style="auto" />
     // </View>
     // <SingleCampsite campsite={campsite}/>
-    <Campsites />
     // <SingleCampsite />
     // <Calendar />
   );
--- front_end/src/screens/homepage/Homepage.js
+++ front_end/src/screens/homepage/Homepage.js
@@ -4,31 +4,31 @@ import axios from 'axios';
 import SingleCampsite from '../campsite/SingleCampsite.js';
 
 const Campsites = () => {
-
   const [campsites, setCampsites] = useState([]);
 
   useEffect(() => {
-    axios.get('http://192.168.1.3:3000/campsites')
+    axios
+      .get('http://10.0.0.30:3000/campsites')
       .then((results) => {
         console.log('results from successful axios request', results.data);
         setCampsites(results.data);
       })
       .catch((error) => {
         console.log('error', error);
-      })
+      });
   }, []);
 
   const handleReserve = () => {
-    console.log('you clicked the reserve button')
-  }
+    console.log('you clicked the reserve button');
+  };
 
   return (
     <View>
       {campsites.map((campsite) => {
-        return <SingleCampsite campsite={campsite}/>
+        return <SingleCampsite campsite={campsite} />;
       })}
     </View>
   );
-}
+};
 
 export default Campsites;
