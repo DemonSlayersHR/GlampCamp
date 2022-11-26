e2bb419ab2d8127d80c70a85079dfb7e36b9271c
Scott Matsuda <smatsuda2014@gmail.com>
2022-11-26 14:45:00 -0800

    navigation to single campsite

--- front_end/src/screens/homepage/Homepage.js
+++ front_end/src/screens/homepage/Homepage.js
@@ -25,7 +25,7 @@ export default function Homepage({ navigation }) {
     <View style={styles.container}>
       <Search navigation={navigation}/>
       <Filter setFilter={setFilter} filter={filter}/>
-      <Feed campsites={campsites}/>
+      <Feed navigation={navigation} campsites={campsites}/>
       <Nav navigation={navigation}/>
     </View>
   );
